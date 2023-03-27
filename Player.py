93e7d111e84620aff7cfebd8ce248eb320fa8c22
Ethan Blood <ethan.blood@hotmail.co.uk>
2023-03-27 15:03:41 +0100

    Completing gui

--- Player.py
+++ Player.py
@@ -2,9 +2,13 @@ from Genome import *
 import Mutation
 import Listen
 import Export
+from functools import partial
+import tkinter
+from tkinter import *
 
 POPULATIONSIZE = 5
 LENGTH = 2
+individual = 0
 
 population = [Genome(length=LENGTH) for each in range(POPULATIONSIZE)]
 
@@ -25,26 +29,59 @@ for genome in population:
         phrase._shape()
     genome.repitition()
 
-while True:
-    used = []
-    for each in population:
-        Listen.playSequence(each)
-        
-        while True:
-            choice = (input("rate melody between 1 and 0 "))
-            if choice.isnumeric():
-                choice = int(choice)
-                if choice > 1 or choice < 0:
-                    print("incorrect range")
-                else:
-                    each.Fitness = choice
-                    break
+def __main__():
+    root = Tk()
+    root.title('eMC Player')
 
-            else:
-                if 'save' in choice:
-                    arguments = choice.split(" ")
-                    arguments.remove('save')
-                    Export.as_midi(each,harmony = True)
+    def playSequence():
+        global individual
+        Listen.playSequence(population[individual])
+        if individual == len(population):
+            Mutation.crossover(population)
+            individual = 0
+        return
 
-    if Mutation.crossover(population):
-        print("Success!")
+    def Save():
+        Export.as_midi(population[individual],harmony = True)
+        return
+
+    def passIndividual():
+        global individual
+        population[individual].Fitness = 1
+        individual += 1
+
+        if individual == len(population):
+            Mutation.crossover(population)
+            individual = 0
+        return
+    
+    def failIndividual():
+        global individual
+        population[individual].Fitness = 0
+        individual += 1
+
+        if individual == len(population):
+            Mutation.crossover(population)
+            individual = 0
+        return
+    
+    play = Button(root, text='Play',command=lambda:playSequence())
+    play.grid(row=0,column=0)
+
+    save = Button(root, text='Save',command=lambda:Save())
+    save.grid(row=0,column=1)
+
+    good = Button(root,text='Pass',command=lambda:passIndividual())
+    good.grid(column=0,row=1)
+
+    bad = Button(root,text='Fail',command=lambda:failIndividual())
+    bad.grid(column=1,row=1)
+
+    indLabel = Label(root,text=str(individual))
+    indLabel.grid(column=2,row=0)
+    root.mainloop()
+
+
+if __name__ == '__main__':
+    individual = 0
+    __main__()
