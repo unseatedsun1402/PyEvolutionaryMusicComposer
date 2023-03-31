7d111ec7e26a440eacae200338405d9e7cfd1c3f
Ethan Blood <ethan.blood@hotmail.co.uk>
2023-03-31 15:13:48 +0100

    added indifferent

--- Player.py
+++ Player.py
@@ -80,6 +80,35 @@ def __main__():
         parentsLable.configure(text='Parents: '+str(parentPool))
         return
     
+    def chanceIndividual():
+        '''Assigns a possible pass to the fitness score'''
+        global individual
+        if random.random() > 0.5:
+            population[individual].Fitness = 1 
+            global parentPool
+            parentPool += 1
+        else:
+            population[individual].Fitness = 0 
+        
+        individual += 1
+        
+        
+        if individual == len(population):
+            if Mutation.crossover(population):
+                print('Success')
+                fitnessModel.analysePopulation(population)
+                global trainingcycles
+                trainingcycles -= 1
+                if trainingcycles == 0:
+                    testAlgorithm()
+                    return                
+
+            individual = 0
+            parentPool = 0
+        indLabel.configure(text='Individual: '+str(individual+1))
+        parentsLable.configure(text='Parents: '+str(parentPool))
+        return
+    
     def failIndividual():
         '''does not add indiviual to parent pool'''
         global individual
@@ -117,24 +146,28 @@ def __main__():
     good.configure(bg='#ef6f6c',highlightthickness=0, bd=0)
     good.grid(column=0,row=1,sticky='EW')
 
+    neutral = Button(frame,text='Indifferent',command=lambda:chanceIndividual())
+    neutral.configure(bg='#ef6f6c',highlightthickness=0, bd=0)
+    neutral.grid(column=1,row=1,sticky='EW')
+
     bad = Button(frame,text='Fail',command=lambda:failIndividual())
     bad.configure(bg='#ef6f6c',highlightthickness=0, bd=0)
-    bad.grid(column=1,row=1,sticky='EW')
+    bad.grid(column=2,row=1,sticky='EW')
 
     indLabel = Label(frame,text='Individual: '+str(individual+1))
     indLabel.configure(bg='#ef6f6c')
-    indLabel.grid(column=2,row=0,sticky='EW')
+    indLabel.grid(column=3,row=0,sticky='EW')
 
     parentsLable = Label(frame,text='Parents: '+ str(parentPool))
     parentsLable.configure(bg='#ef6f6c')
-    parentsLable.grid(column=2,row=1,sticky='EW')
+    parentsLable.grid(column=3,row=1,sticky='EW')
 
     ###################   Tempo Slider   #################
     tmp = Variable
     tempoSlider = Scale(frame,digits=3,from_=180,to=40, variable=tmp,command=lambda x:Listen.setTempo(int(x)))
     tempoSlider.configure(bg='#ef6f6c')
     tempoSlider.set(120)
-    tempoSlider.grid(row=0,column=3,rowspan=2)
+    tempoSlider.grid(row=0,column=4,rowspan=2)
     
 
     #################### Draw the keyboard ###############
